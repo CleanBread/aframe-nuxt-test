a0d7ad340598af1efe222ab871b24f44b50ba5ab
MacRat <m@crat.jp>
2019-04-17 22:18:09 +0900

    Enhanced

---
+++ README.md
@@ -0,0 +1,15 @@
+# [A-Frame](https://aframe.io/) / [Nuxt.js](https://nuxtjs.org) test
+
+## try
+try on [GitHub pages](https://macrat.github.io/aframe-nuxt-test/)
+
+## get started
+
+``` shell
+$ git clone https://github.com/macrat/aframe-nuxt-test.git
+$ cd aframe-nuxt-test
+$ npm install
+$ npm start
+```
+
+and open http://localhost:3000 with your browser.
--- pages/index.vue
+++ pages/index.vue
@@ -12,55 +12,91 @@ a-scene {
     z-index: -1;
 }
 
-#elements {
+#text-element {
+    position: absolute;
+    width: 800px;
+    height: 800px;
+    padding: 8px;
+}
+
+#ar-elements {
     position: fixed;
     top: 0;
     left: 0;
-    overflow: hidden;
     z-index: -100;
-	font-size: 400%;
-}
-
-#teb {
-    position: absolute;
-    width: 800px;
-    height: 600px;
-    padding: 8px;
+    font-size: 400%;
 }
 
 main {
     box-sizing: border-box;
-    margin-top: 90vh;
+    margin-top: 80vh;
     width: 100%;
     padding: 1em;
     background-color: white;
+
+    line-height: 1.3;
+    display: flex;
+    justify-content: center;
+}
+
+article {
+    max-width: 80em;
 }
 </style>
 
 <template>
     <div>
-        <a-scene embedded vr-mode-ui="enabled: false" v-pre>
-            <a-assets v-pre>
-                <img id=tea src="~assets/tea.png">
-            </a-assets>
+        <div ref=scene>
+            <a-scene embedded vr-mode-ui="enabled: false" v-pre>
+                <a-assets v-pre>
+                    <img id=picture src="~assets/tea.png">
+                </a-assets>
 
-            <a-plane src="#tea" position="0 0 -4" rotation="-90 0 0" width=4 height=4 />
-            <a-plane material="shader: html; target: #teb; fps: 30; transparent: true" width=3 height=2 position="0 1 -4" rotation="0 0 0" />
-            <a-sky color="#ECECEC" />
-        </a-scene>
+                <a-plane src="#picture" position="0 0 -4" rotation="-90 0 0" width=4 height=4 />
+                <a-plane id=text-plane material="shader: html; target: #text-element; fps: 30; transparent: true" width=3 height=3 position="0 1.7 -4" rotation="0 0 0" />
 
-        <div id=elements>
-            <div id=teb :style=style>
-                <h1>hello world!</h1>
-                this is a teb<br>
-                <span v-html=text />
+                <a-sky color="#eee" />
+            </a-scene>
+        </div>
+p
+        <div id=ar-elements>
+            <div id=text-element :style="{color: color}">
+                <h1>A-Frame<br>Nuxt.js test</h1>
+                <p>this is a test of A-Frame and Nuxt.js</p>
+                <p>
+                    <!-- XXX: didn't work mustache data binding. why? -->
+                    current color: <span v-html=color /><br>
+                    current position: <span v-html=position.y />
+
+                </p>
             </div>
         </div>
 
         <main>
-            <h1>hello world</h1>
-            <p>this is a test site</p>
-            <p><input v-model=text /></p>
+            <article>
+                <h1>A-Frame / Nuxt.js test</h1>
+                <p>
+                    <label>color: <select v-model=color>
+                        <option value="red">red</option>
+                        <option value="green">green</option>
+                        <option value="blue">blue</option>
+                    </select></label><br>
+                    <label>position Y: <input type=range min=0 max=3 step=0.1 v-model=position.y></label>
+                </p>
+                <section>
+                    <h2>how to get started</h2>
+                    <ol>
+                        <li>git clone <a href="https://github.com/macrat/aframe-nuxt-test">https://github.com/macrat/aframe-nuxt-test.git</a> &amp;&amp; cd aframe-nuxt-test</li>
+                        <li>npm install</li>
+                        <li>npm start</li>
+                        <li>open <a href="http://localhost:3000">http://localhost:3000</a></li>
+                    </ol>
+                </section>
+                <section>
+                    <h2>dummy text</h2>
+                    <p v-for="_ in [...new Array(30)]">dummy text for scroll</p>
+                </section>
+            </article>
         </main>
     </div>
 </template>
@@ -75,14 +111,16 @@ export default {
     },
     data() {
         return {
-            text: 'foobar',
+            color: 'red',
+            position: {x: 0, y: 1.7, z: -4},
         };
     },
-    computed: {
-        style() {
-            return {
-                'color': (this.text === 'foobar') ? 'red' : (this.text ? 'green' : 'blue'),
-            };
+    watch: {
+        position: {
+            handler(val) {
+                this.$refs.scene.querySelector('#text-plane').setAttribute('position', val);
+            },
+            deep: true,
         },
     },
 };
