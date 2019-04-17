8faeea708539a443410c0bc1d1e8071640d9df3e
MacRat <m@crat.jp>
2019-04-17 22:46:56 +0900

    Added header tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,13 @@ const router = process.env.NODE_ENV === 'production' ? {
 } : {};
 
 export default {
+    head: {
+        title: 'A-Frame / Nuxt.js test',
+        meta: [
+            {charset: 'utf-8'},
+            {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+        ],
+    },
     generate: {
         dir: 'docs',
     },
